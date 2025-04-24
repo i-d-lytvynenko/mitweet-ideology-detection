f653c4b0efb34d8e9ce72b8a055f05e9bf2cc855
Ivan Lytvynenko <lytvynenko.i.d@gmail.com>
2025-04-24 18:23:27 +0300

    style: tweak formatting

--- src/training/data/dataset_info.py
+++ src/training/data/dataset_info.py
@@ -13,34 +13,20 @@ facets = {
     "Personal Right (PeR)": ("Social Responsibility", "Individual Right"),
 }
 
+# fmt: off
 facet_abbrevs = [
-    "PoR",
-    "SS",
-    "EO",
-    "EE",
-    "EP",
-    "CSR",
-    "CV",
-    "DS",
-    "MF",
-    "SD",
-    "JO",
-    "PeR",
+    "PoR", "SS", "EO", "EE", "EP", "CSR",
+    "CV",  "DS", "MF", "SD", "JO", "PeR",
 ]
+# fmt: on
 
 # Best F1 scores from https://aclanthology.org/2023.emnlp-main.256
 # Note: irrelevant tweets were not used
+# fmt: off
 paper_results = [
-    "31.79 (STD 9.27)",
-    "33.50 (STD 5.30)",
-    "54.42 (STD 4.97)",
-    "48.51 (STD 1.05)",
-    "53.66 (STD 2.69)",
-    "49.93 (STD 6.07)",
-    "51.77 (STD 11.01)",
-    "57.59 (STD 0.53)",
-    "48.39 (STD 6.02)",
-    "45.00 (STD 2.37)",
-    "38.83 (STD 1.21)",
-    "40.20 (STD 5.67)",
+    "31.79 (STD 9.27)", "33.50 (STD 5.30)", "54.42 (STD 4.97)",
+    "48.51 (STD 1.05)", "53.66 (STD 2.69)", "49.93 (STD 6.07)",
+    "51.77 (STD 11.01)", "57.59 (STD 0.53)", "48.39 (STD 6.02)",
+    "45.00 (STD 2.37)", "38.83 (STD 1.21)", "40.20 (STD 5.67)",
 ]
+# fmt: on
